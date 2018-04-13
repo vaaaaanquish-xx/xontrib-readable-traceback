2af1f4b96bdf72db51dd4f7a5872946c107c8cba
@vaaaaanquish <6syun9@gmail.com>
2018-04-13 16:14:39 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -3,38 +3,27 @@ from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+requires = ['backtrace>=0.2.0', 'colorama>=0.3.7']
+
 setup(
-    name='xontrib-backtrace',
+    name='xontrib-readable-traceback',
     version='0.1.0',
-
-    description='TraceBack easier to see for xonsh.',
+    description='Makc Traceback easier to see for xonsh.',
     long_description=long_description,
-    url='https://git.corp.yahoo.co.jp/shukawai/xontrib-backtrace',
-
+    url='https://github.com/vaaaaanquish/xontrib-readable-traceback',
     author='vaaaaanquish',
     author_email='6syun9@gmail.com',
-
     license='MIT',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
     package_data={'xontrib': ['*.xsh']},
